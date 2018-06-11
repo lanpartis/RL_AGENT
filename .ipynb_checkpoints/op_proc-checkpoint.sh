29216f965b9d70110b6f65510bc1adf97545c93d
lanpartis <jiaoyongcheng@gmail.com>
2018-06-11 23:13:45 +0900

    remove ipytnb_checkpoints

--- .ipynb_checkpoints/op_proc-checkpoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-export LD_LIBRARY_PATH=~/openpose/3rdparty/caffe/build/lib:$LD_LIBRARY_PATH
-alias openpose='~/openpose/build/examples/openpose/openpose.bin'
-dir=$1
-openpose --image_dir $dir --write_json $dir
